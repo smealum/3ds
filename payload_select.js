54a6406b8b9a1124d28b3da3a84c51755d1e83a6
smea <smealum@gmail.com>
2017-08-19 15:15:19 -0700

    o3ds 11.4+

--- payload_select.js
+++ payload_select.js
@@ -320,7 +320,9 @@ window.onload=function(){
             '0': zeroMicro_1,
             '1': zeroMicro_1,
             '2': zeroMicro_1,
-            '3': zeroMicro_1
+            '3': zeroMicro_1,
+            '4': zeroMicro_1,
+            '5': zeroMicro_1
         }
     }
 
